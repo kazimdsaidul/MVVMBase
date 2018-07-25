ca0b2100b3b2a68e64e4585f48065acfb1713548
Kazi Md. Saddul <kazimdsaidul@gmail.com>
2018-07-25 20:54:46 +0600

    change xml file

--- app/src/main/java/saidul/com/mvvmbase/views/main/BasicActivity.kt
+++ app/src/main/java/saidul/com/mvvmbase/views/main/BasicActivity.kt
@@ -4,7 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_basic.*
 import saidul.com.mvvmbase.R
 import saidul.com.mvvmbase.common.BaseActivity
 import timber.log.Timber
@@ -18,7 +18,7 @@ class BasicActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_basic)
 
         mBasicViewModel = ViewModelProviders.of(this, viewModelFactory).get(BasicViewModel::class.java)
 
