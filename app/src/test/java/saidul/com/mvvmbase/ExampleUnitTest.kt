48cf92632fdc644b59757a0de4f026b7b88d0a6f
Kazi Md. Saddul <kazimdsaidul@gmail.com>
2018-07-30 00:53:21 +0600

    start working rx java zip operator example

--- app/src/test/java/saidul/com/mvvmbase/ExampleUnitTest.kt
+++ app/src/test/java/saidul/com/mvvmbase/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package saidul.com.mvvmbase
 
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +17,30 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun rxZip(){
+
+        val stringObservable1 = Observable.just("Hello")
+        val stringObservable2 = Observable.just(arrayOf("1","2","3"))
+
+        Observable.zip(stringObservable1, stringObservable2, object : BiFunction<String, Array<String>, String> {
+            @Throws(Exception::class)
+            override fun apply( s: String, s2: Array<String>): String {
+                println("apply "+Thread.currentThread().name)
+                return "$s - $s2"
+            }
+        }).observeOn(Schedulers.io())
+                .subscribe(object : Consumer<String> {
+            @Throws(Exception::class)
+            override fun accept(s: String) {
+
+                println("accept "+Thread.currentThread().name)
+                println(s)
+            }
+        })
+
+
     }
 }
